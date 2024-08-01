55abc2183972cffef4a3581b6417aa7fd8923525
achangelia1 <achangelia@onaim.io>
2024-08-01 12:34:45 +0400

    added projects

---
+++ RNG/RNG.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+namespace RNG;
+
+public static class RNG
+{
+    public static int Next(int exclusive) { return RandomNumberGenerator.GetInt32(exclusive); }
+    public static int NextInMillion() { return RandomNumberGenerator.GetInt32(1_000_000); }
+}
---
+++ Shared.ServiceRegistry/DependencyResolver.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.ServiceRegistry;
+
+public class DependencyResolver
+{
+
+}
