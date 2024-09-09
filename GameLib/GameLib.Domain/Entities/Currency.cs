bee350c6c8e03471a9ebe6717c7331946a6c0bf4
achangelia1 <achangelia@onaim.io>
2024-09-09 08:39:39 +0400

    Remove symbol from currency of gamelib

--- GameLib/GameLib.Domain/Entities/Currency.cs
+++ GameLib/GameLib.Domain/Entities/Currency.cs
@@ -5,7 +5,6 @@ namespace GameLib.Domain.Entities;
 public class Currency : BaseEntity<string>
 {
     public string Name { get; set; }
-    public string Symbol { get; set; }
     public ICollection<Price> Prices { get; set; }
     public ICollection<PrizeType> PrizeTypes { get; set; }
 }
