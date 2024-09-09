e330f2eb3e7a5841a0c9ed37b60d84e3e9e11165
achangelia1 <achangelia@onaim.io>
2024-09-09 14:33:03 +0400

    Hub: refactor domain entities to initialize properties via constructor and restrict property setters (#4)

--- Hub/Hub.Domain/Entities/Player.cs
+++ Hub/Hub.Domain/Entities/Player.cs
@@ -1,11 +1,33 @@
-﻿using Shared.Domain.Entities;
+﻿#nullable disable
+
+using Shared.Domain.Entities;
 
 namespace Hub.Domain.Entities;
 
 public class Player : BaseEntity<int>
 {
-    public string UserName { get; set; }
-    public List<int> SegmentIds { get; set; }
+    public Player()
+    {
 
-    public ICollection<PlayerBalance> PlayerBalances { get; set; }
+    }
+
+    public Player(int id, string userName, List<int>? segmentIds = null, ICollection<PlayerBalance>? playerBalances = null)
+    {
+        Id = id;
+        UserName = userName;
+        SegmentIds = segmentIds;
+        PlayerBalances = playerBalances;
+    }
+
+    public string UserName { get; private set; }
+    public List<int>? SegmentIds { get; private set; }
+
+    public ICollection<PlayerBalance>? PlayerBalances { get; set; }
+
+    public void ChangeDetails(string userName, List<int>? segmentIds = null, ICollection<PlayerBalance>? playerBalances = null)
+    {
+        UserName = userName;
+        SegmentIds = segmentIds;
+        PlayerBalances = playerBalances;
+    }
 }
--- Hub/Hub.Domain/Entities/Transaction.cs
+++ Hub/Hub.Domain/Entities/Transaction.cs
@@ -1,29 +1,53 @@
-﻿using Shared.Domain.Entities;
+﻿#nullable disable
+
+using Shared.Domain.Entities;
 
 namespace Hub.Domain.Entities;
 
 public class Transaction : BaseEntity<int>
 {
-    public decimal Amount { get; set; }
+    public Transaction()
+    {
+        
+    }
 
-    public int GameId { get; set; }
-    public Game Game { get; set; }
+    public Transaction(decimal amount, int gameId, int playerId, AccountType fromAccountId, AccountType toAccountId, string currencyId, TransactionStatus status, TransactionType type)
+    {
+        Amount = amount;
+        GameId = gameId;
+        PlayerId = playerId;
+        FromAccount = fromAccountId;
+        ToAccount = toAccountId;
+        CurrencyId = currencyId;
+        Status = status;
+        Type = type;
+    }
 
-    public int FromAccountId { get; set; }
-    public AccountType FromAccount { get; set; }
+    public decimal Amount { get; private set; }
 
-    public int ToAccountId { get; set; }
-    public AccountType ToAccount { get; set; }
+    public int GameId { get; private set; }
+    public Game Game { get; private set; }
 
-    public int PlayerId { get; set; }
-    public Player Player { get; set; }
+    public int PlayerId { get; private set; }
+    public Player Player { get; private set; }
 
-    public string CurrencyId { get; set; }
-    public Currency Currency { get; set; }
+    public int FromAccountId { get; private set; }
+    public AccountType FromAccount { get; private set; }
 
-    public int StatusId { get; set; }
-    public TransactionStatus Status { get; set; }
+    public int ToAccountId { get; private set; }
+    public AccountType ToAccount { get; private set; }
 
-    public int TypeId { get; set; }
-    public TransactionType Type { get; set; }
+    public string CurrencyId { get; private set; }
+    public Currency Currency { get; private set; }
+
+    public int StatusId { get; private set; }
+    public TransactionStatus Status { get; private set; }
+
+    public int TypeId { get; private set; }
+    public TransactionType Type { get; private set; }
+
+    public void SetStatus(TransactionStatus status)
+    {
+        Status = status;
+    }
 }
