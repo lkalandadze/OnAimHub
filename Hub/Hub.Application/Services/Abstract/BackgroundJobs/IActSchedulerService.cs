97df946986c6d3baa9bb6a32e0cc15dd7417bd12
AkakiKokaiaON <akokaia@onaim.io>
2024-10-21 15:31:07 +0400

    finished jobs

---
+++ Hub/Hub.Application/Services/Abstract/BackgroundJobs/IActSchedulerService.cs
@@ -0,0 +1,13 @@
+﻿using Hub.Domain.Entities;
+using Hub.Domain.Enum;
+
+namespace Hub.Application.Services.Abstract.BackgroundJobs;
+
+public interface IActSchedulerService
+{
+    void ScheduleActJobs(Act act);
+
+    Task StartAct(int actId);
+
+    Task FinishAct(int actId);
+}
