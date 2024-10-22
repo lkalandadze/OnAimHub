91d14abfc49a11519054b874e531bf57e6d177a7
achangelia1 <achangelia@onaim.io>
2024-10-22 13:47:15 +0400

    Refactor gamelib services and repositories

--- GameLib/GameLib.Application/Controllers/HubController.cs
+++ GameLib/GameLib.Application/Controllers/HubController.cs
@@ -1,47 +1,175 @@
 ﻿using GameLib.Application.Models.Configuration;
 using GameLib.Application.Models.Game;
+using GameLib.Application.Models.PrizeType;
+using GameLib.Application.Models.Segment;
 using GameLib.Application.Services.Abstract;
-using GameLib.Application.Services.Concrete;
-using Microsoft.AspNetCore.Http;
+using GameLib.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameLib.Application.Controllers;
 
 public class HubController : BaseApiController
 {
-    private readonly IGameInfoService _gameInfoService;
-    private readonly IConfigurationService _configurationService;
+    private readonly IGameService _gameService;
+    private readonly IGameConfigurationService _configurationService;
+    private readonly ISegmentService _segmentService;
+    private readonly IPrizeTypeService _prizeTypeService;
 
-    public HubController(IConfigurationService configurationService, IGameInfoService gameInfoService)
+    public HubController(IGameService gameService, IGameConfigurationService configurationService, ISegmentService segmentService, IPrizeTypeService prizeTypeService)
     {
+        _gameService = gameService;
         _configurationService = configurationService;
-        _gameInfoService = gameInfoService;
+        _segmentService = segmentService;
+        _prizeTypeService = prizeTypeService;
     }
 
+    #region Game
+
+    [HttpGet(nameof(GameShortInfo))]
+    public async Task<ActionResult<IEnumerable<GetGameShortInfoModel>>> GameShortInfo()
+    {
+        return Ok(await _gameService.GetGameShortInfo());
+    }
+
+    [HttpGet(nameof(GameStatus))]
+    public IActionResult GameStatus()
+    {
+        return Ok(_gameService.GameStatus());
+    }
+
+    [HttpPost(nameof(ActivateGame))]
+    public IActionResult ActivateGame()
+    {
+        _gameService.ActivateGame();
+
+        return Ok(new { Message = "Game activated." });
+    }
+
+    [HttpPost(nameof(DeactivateGame))]
+    public IActionResult DeactivateGame()
+    {
+        _gameService.DeactivateGame();
+
+        return Ok(new { Message = "Game deactivated." });
+    }
+
+    #endregion
+
+    #region Configurations
+
     [HttpGet(nameof(Configurations))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<IEnumerable<ConfigurationBaseGetModel>>> Configurations()
     {
         return Ok(await _configurationService.GetAllAsync());
     }
 
-    [HttpGet("Configurations/{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    public async Task<ActionResult<ConfigurationBaseGetModel>> GetConfigurationByIdAsync([FromRoute] int id)
+    [HttpGet(nameof(ConfigurationById))]
+    public async Task<ActionResult<GameConfiguration>> ConfigurationById(int id)
     {
-        return Ok(await _configurationService.GetConfigurationByIdAsync(id));
+        return Ok(await _configurationService.GetByIdAsync(id));
     }
 
-    [HttpGet(nameof(GameShortInfo))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    public async Task<ActionResult<IEnumerable<GetGameShortInfoModel>>> GameShortInfo()
+    [HttpPost(nameof(CreateConfiguration))]
+    public async Task<ActionResult> CreateConfiguration([FromBody] ConfigurationCreateModel model)
     {
-        return Ok(await _gameInfoService.GetGameShortInfo());
+        await _configurationService.CreateAsync(model);
+        return StatusCode(201);
     }
+
+    [HttpPut(nameof(UpdateConfiguration))]
+    public async Task<ActionResult> UpdateConfiguration([FromRoute] int id, [FromBody] ConfigurationUpdateModel model)
+    {
+        await _configurationService.UpdateAsync(id, model);
+        return StatusCode(200);
+    }
+
+    [HttpPatch(nameof(ActivateConfiguration))]
+    public async Task<ActionResult> ActivateConfiguration([FromRoute] int id)
+    {
+        await _configurationService.ActivateAsync(id);
+        return StatusCode(200);
+    }
+
+    [HttpPatch(nameof(DeactivateConfiguration))]
+    public async Task<ActionResult> DeactivateConfiguration([FromRoute] int id)
+    {
+        await _configurationService.DeactivateAsync(id);
+        return StatusCode(200);
+    }
+
+    [HttpPost(nameof(AssignConfigurationToSegments))]
+    public async Task<ActionResult> AssignConfigurationToSegments([FromRoute] int id, [FromBody] IEnumerable<string> segmentIds)
+    {
+        await _configurationService.AssignConfigurationToSegmentsAsync(id, segmentIds);
+        return StatusCode(200);
+    }
+
+    [HttpPost(nameof(UnassignConfigurationToSegments))]
+    public async Task<ActionResult> UnassignConfigurationToSegments([FromRoute] int id, [FromBody] IEnumerable<string> segmentIds)
+    {
+        await _configurationService.UnassignConfigurationToSegmentsAsync(id, segmentIds);
+        return StatusCode(200);
+    }
+
+    #endregion
+
+    #region Segments
+
+    [HttpGet(nameof(Segments))]
+    public async Task<ActionResult<IEnumerable<SegmentBaseGetModel>>> Segments()
+    {
+        return Ok(await _segmentService.GetAllAsync());
+    }
+
+    [HttpGet(nameof(SegmentById))]
+    public async Task<ActionResult<ConfigurationBaseGetModel>> SegmentById(int id)
+    {
+        return Ok(await _segmentService.GetByIdAsync(id));
+    }
+
+    [HttpPost(nameof(CreateSegment))]
+    public async Task<ActionResult> CreateSegment([FromBody] SegmentCreateModel model)
+    {
+        await _segmentService.CreateAsync(model);
+        return StatusCode(201);
+    }
+
+    [HttpPatch(nameof(DeleteSegment))]
+    public async Task<ActionResult> DeleteSegment([FromRoute] int id)
+    {
+        await _segmentService.DeleteAsync(id);
+        return StatusCode(201);
+    }
+
+    #endregion
+
+    #region PrizeType
+
+    [HttpGet(nameof(PrizeTypes))]
+    public async Task<ActionResult<IEnumerable<PrizeTypeGetModel>>> PrizeTypes()
+    {
+        return Ok(await _prizeTypeService.GetAllAsync());
+    }
+
+    [HttpGet(nameof(PrizeTypeById))]
+    public async Task<ActionResult<PrizeTypeGetModel>> PrizeTypeById(int id)
+    {
+        return Ok(await _prizeTypeService.GetByIdAsync(id));
+    }
+
+    [HttpPost(nameof(CreatePrizeType))]
+    public async Task<ActionResult> CreatePrizeType([FromBody] PrizeTypeCreateModel model)
+    {
+        await _prizeTypeService.CreateAsync(model);
+        return StatusCode(201);
+    }
+
+    [HttpPut(nameof(UpdatePrizeType))]
+    public async Task<ActionResult> UpdatePrizeType([FromRoute] int id, [FromBody] PrizeTypeUpdateModel model)
+    {
+        await _prizeTypeService.UpdateAsync(id, model);
+        return StatusCode(200);
+    }
+
+    #endregion
 }
--- GameLib/GameLib.Application/Services/Concrete/GameConfigurationService.cs
+++ GameLib/GameLib.Application/Services/Concrete/GameConfigurationService.cs
@@ -4,18 +4,19 @@ using GameLib.Domain.Abstractions;
 using GameLib.Domain.Abstractions.Repository;
 using GameLib.Domain.Entities;
 using GameLib.Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameLib.Application.Services.Concrete;
 
-public class ConfigurationService : IConfigurationService
+public class GameConfigurationService : IGameConfigurationService
 {
     private readonly IGameConfigurationRepository _configurationRepository;
     private readonly ISegmentRepository _segmentRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly SharedGameConfigDbContext _dbContext;
 
-    public ConfigurationService(IGameConfigurationRepository configurationRepository, ISegmentRepository segmentRepository, IUnitOfWork unitOfWork, SharedGameConfigDbContext dbContext)
+    public GameConfigurationService(IGameConfigurationRepository configurationRepository, ISegmentRepository segmentRepository, IUnitOfWork unitOfWork, SharedGameConfigDbContext dbContext)
     {
         _configurationRepository = configurationRepository;
         _segmentRepository = segmentRepository;
@@ -23,17 +24,15 @@ public class ConfigurationService : IConfigurationService
         _dbContext = dbContext;
     }
 
-    public async Task<IEnumerable<ConfigurationGetModel>> GetAllAsync()
+    public async Task<IEnumerable<ConfigurationBaseGetModel>> GetAllAsync()
     {
-        var configurations = await _configurationRepository.Query()
-                                                           .Include(c => c.Segments)
-                                                           .Where(c => c.Segments.Any(s => !s.IsDeleted))
-                                                           .ToListAsync();
+        var configurations = await _configurationRepository.Query().ToListAsync();
+        var mappedConfigurations = configurations.Select(c => ConfigurationBaseGetModel.MapFrom(c));
 
-        return configurations.Select(c => ConfigurationGetModel.MapFrom(c));
+        return mappedConfigurations == null ? Enumerable.Empty<ConfigurationBaseGetModel>() : mappedConfigurations;
     }
 
-    public async Task<ConfigurationGetModel> GetByIdAsync(int id)
+    public async Task<GameConfiguration> GetByIdAsync(int id)
     {
         var configuration = await _configurationRepository.OfIdAsync(id);
 
@@ -42,7 +41,7 @@ public class ConfigurationService : IConfigurationService
             throw new KeyNotFoundException($"Configuration not found for Id: {id}");
         }
 
-        return ConfigurationGetModel.MapFrom(configuration);
+        return configuration;
     }
 
     public async Task CreateAsync(ConfigurationCreateModel model)
@@ -143,70 +142,5 @@ public class ConfigurationService : IConfigurationService
 
         _configurationRepository.Update(configuration);
         await _unitOfWork.SaveAsync();
-    }
-
-    public async Task<GameConfiguration?> GetConfigurationByIdAsync(int id)
-    {
-        GameConfiguration? result = null;
-
-        // Iterate over all DbSets in the context
-        var dbSets = _dbContext.GetType().GetProperties()
-            .Where(p => p.PropertyType.IsGenericType &&
-                        p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
-
-        foreach (var dbSetProperty in dbSets)
-        {
-            // Get the entity type of the DbSet (e.g., WheelConfiguration)
-            var entityType = dbSetProperty.PropertyType.GetGenericArguments()[0];
-
-            // Check if the entity type inherits from GameConfiguration
-            if (typeof(GameConfiguration).IsAssignableFrom(entityType))
-            {
-                // Get the DbSet as IQueryable
-                var dbSet = dbSetProperty.GetValue(_dbContext) as IQueryable;
-
-                if (dbSet != null)
-                {
-                    // Cast to IQueryable<GameConfiguration>
-                    var typedQuery = dbSet.Cast<GameConfiguration>();
-
-                    // Include navigation properties dynamically
-                    var queryWithIncludes = IncludeNavigationProperties(typedQuery);
-
-                    // Query the DbSet for the entity with the specified ID
-                    result = await queryWithIncludes.FirstOrDefaultAsync(gc => gc.Id == id);
-
-                    // If a result is found, return it
-                    if (result != null)
-                    {
-                        return result;
-                    }
-                }
-            }
-        }
-
-        return result; // Return null if no configuration was found
-    }
-
-    private IQueryable<GameConfiguration> IncludeNavigationProperties(IQueryable<GameConfiguration> query)
-    {
-        // Get all entity types in the model that inherit from GameConfiguration
-        var derivedEntityTypes = _dbContext.Model.GetEntityTypes()
-            .Where(e => typeof(GameConfiguration).IsAssignableFrom(e.ClrType) && !e.ClrType.IsAbstract);
-
-        // Include navigation properties for each derived type
-        foreach (var entityType in derivedEntityTypes)
-        {
-            // Get all navigation properties for the derived entity
-            var navigationProperties = entityType.GetNavigations().Select(n => n.Name).Distinct();
-
-            // Include each navigation property dynamically
-            foreach (var navigationProperty in navigationProperties)
-            {
-                query = query.Include(navigationProperty);
-            }
-        }
-
-        return query;
     }
 }
