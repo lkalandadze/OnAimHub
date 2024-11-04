c65293dab628add4dc2254fa36d7cae7402571ed
AkakiKokaiaON <akokaia@onaim.io>
2024-11-04 14:29:27 +0400

    removed unusued data types

---
+++ Leaderboard/Leaderboard.Infrastructure/Migrations/20241104102909_Removed_Unused_DataTypes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Leaderboard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removed_Unused_DataTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobType",
+                table: "LeaderboardTemplate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JobType",
+                table: "LeaderboardTemplate",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
