b30945664be3d6bfc8e164352c641e9a26743810
achangelia1 <achangelia@onaim.io>
2024-11-22 10:52:33 +0400

    Fix withdraw option type configuration

--- Core/Hub/Hub.Infrastructure/TypeConfigurations/WithdrawOptionTypeConfiguration.cs
+++ Core/Hub/Hub.Infrastructure/TypeConfigurations/WithdrawOptionTypeConfiguration.cs
@@ -9,7 +9,7 @@ public class WithdrawOptionTypeConfiguration : IEntityTypeConfiguration<Withdraw
 {
     public void Configure(EntityTypeBuilder<WithdrawOption> builder)
     {
-        // Configuration for WithdrawOptionGroups
+        // Many-to-Many Relationship between WithdrawOption and WithdrawOptionGroups
         builder.HasMany(w => w.WithdrawOptionGroups)
                .WithMany(g => g.WithdrawOptions)
                .UsingEntity<Dictionary<string, object>>(
@@ -24,6 +24,7 @@ public class WithdrawOptionTypeConfiguration : IEntityTypeConfiguration<Withdraw
                         .OnDelete(DeleteBehavior.Cascade)
                 );
 
+        // Many-to-Many Relationship between WithdrawOption and CoinTemplates
         builder.HasMany(w => w.CoinTemplates)
            .WithMany(c => c.WithdrawOptions)
            .UsingEntity<Dictionary<string, object>>(
@@ -37,5 +38,20 @@ public class WithdrawOptionTypeConfiguration : IEntityTypeConfiguration<Withdraw
                     .HasForeignKey($"{nameof(WithdrawOption)}{nameof(WithdrawOption.Id)}")
                     .OnDelete(DeleteBehavior.Cascade)
             );
+
+        // Many-to-Many Relationship between WithdrawOption and PromotionCoins
+        builder.HasMany(w => w.PromotionCoins)
+          .WithMany(c => c.WithdrawOptions)
+          .UsingEntity<Dictionary<string, object>>(
+               $"{nameof(WithdrawOption)}{nameof(PromotionCoin)}Mappings",
+               j => j.HasOne<PromotionCoin>()
+                   .WithMany()
+                   .HasForeignKey($"{nameof(PromotionCoin)}{nameof(PromotionCoin.Id)}")
+                   .OnDelete(DeleteBehavior.Cascade),
+               j => j.HasOne<WithdrawOption>()
+                   .WithMany()
+                   .HasForeignKey($"{nameof(WithdrawOption)}{nameof(WithdrawOption.Id)}")
+                   .OnDelete(DeleteBehavior.Cascade)
+          );
     }
 }
