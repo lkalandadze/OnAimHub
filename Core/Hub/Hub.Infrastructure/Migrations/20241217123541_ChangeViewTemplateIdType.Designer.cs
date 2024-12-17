1ae0c049e7d4d3f5ecbfac2feb1af61be210c66e
achangelia1 <achangelia@onaim.io>
2024-12-17 16:39:16 +0400

    Add migration

--- Core/Hub/Hub.Infrastructure/Migrations/20241217123541_ChangeViewTemplateIdType.Designer.cs
+++ Core/Hub/Hub.Infrastructure/Migrations/20241217123541_ChangeViewTemplateIdType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Hub.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hub.Infrastructure.Migrations
 {
     [DbContext(typeof(HubDbContext))]
-    partial class HubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217123541_ChangeViewTemplateIdType")]
+    partial class ChangeViewTemplateIdType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Core/Hub/Hub.Infrastructure/Migrations/20241217123541_ChangeViewTemplateIdType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeViewTemplateIdType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FromTemplateId",
+                table: "PromotionViews",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FromTemplateId",
+                table: "PromotionViews",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
