dcbdc4df5a342feb11f079270a27a1e981ebee95
GvantsaO <gokropiridze@onaim.io>
2024-12-23 16:43:21 +0400

    aggregation service

---
+++ CoreModules/AggregationService/AggregationService.Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using AggregationService.Infrastructure.Persistance.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AggregationService.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AggregationServiceContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnectionString"));
+        });      
+
+        return services;
+    }
+}
