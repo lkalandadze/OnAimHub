87e9e1b25398d2f6413bef4ea0c6f067bb3a8ed7
GvantsaO <gokropiridze@onaim.io>
2024-12-24 09:53:48 +0400

    added aggregation to docker

--- CoreModules/AggregationService/AggregationService.API/Program.cs
+++ CoreModules/AggregationService/AggregationService.API/Program.cs
@@ -1,11 +1,13 @@
 using OnAim.Admin.Infrasturcture;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost"));
+builder.Services.AddHttpClient();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
